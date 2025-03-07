6bbe918da81a52d3c1ff441b6408f81836525de4
paulmwatson <github.com@paulmwatson.com>
2025-03-07 12:41:43 +0200

    Once more with feeling

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main>
       <div>Next.js on Railway!</div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate non praesentium aliquam quae quo sequi at deleniti distinctio nam explicabo sunt expedita quia minus labore fugiat voluptas, laboriosam earum omnis!</p>
     </main>
   );
 }
